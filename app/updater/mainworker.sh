06ff7b42235a578c652f40fe2dadefc09e0dc665
TamaGoes0711 <tamatama@tensi.org>
2021-05-16 21:43:39 +0900

    Update mainworker.sh

--- app/updater/mainworker.sh
+++ app/updater/mainworker.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
 ###### This is main file ######
+
+## To prevent no-wanted rewrite echo
+echo
+
+## echo commands
+function infoecho(echotxt){
+  echo -en "\r"echotext"\n"
+}
+
+function progress(percentage){
+  echo -en "\rProgress: "percentage"%"
+}
+
+function en(){
+  ## Check for the new update
+  infoecho("Checking for update")
